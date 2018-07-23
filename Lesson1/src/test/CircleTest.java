b26ad5b77382878da787dc32c666693f4646fe10
Vova <Vova@192.168.0.102>
2018-07-23 22:12:30 +0300

    add CircleTest junit test

---
+++ Lesson1/src/test/CircleTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.lessons.circle.Circle;
+
+public class CircleTest {
+
+	@Test
+	public void test() {
+		Circle test = new Circle();
+		test.setRadius(20);
+		int result = test.getArea();
+		assertEquals(1256, result);
+	}
+
+}
