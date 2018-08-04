4c319ec8135f5296be7c2e08f99098f5bb02fc2e
Vova <Vova@192.168.0.101>
2018-08-04 20:12:31 +0300

    fix variables at NumOperations

--- Lesson1/src/com/lessons/numoperations/NumOperations.java
+++ Lesson1/src/com/lessons/numoperations/NumOperations.java
@@ -3,60 +3,60 @@ package com.lessons.numoperations;
 public class NumOperations {
 
 	public int sumNum(int num){
-		String str = Integer.toString(num);
-		int sum = 1;
+		String numToString = Integer.toString(num);
+		int multiplier = 1;
 		int result = 0;
-		for(int i = 0; i < str.length();i++) {
-			sum = sum *10;
-			int var = (num%sum) / (sum/10);
+		for(int i = 0; i < numToString.length();i++) {
+			multiplier = multiplier *10;
+			int var = (num%multiplier) / (multiplier/10);
             result = result + var;  
 		}
 		return result;
 	}
 	
 	public int charSum(int num) {
-		String str = Integer.toString(num);
-		char [] chars = str.toCharArray();
+		String numToString = Integer.toString(num);
+		char [] chars = numToString.toCharArray();
 		int result = 0;
-		String ch = null;
+		String charCase = null;
 		for(int i = 0;i < chars.length;i++) {
-			ch = Character.toString(chars[i]);
-			int num1 = Integer.parseInt(ch);
-			result = result + num1;
+			charCase = Character.toString(chars[i]);
+			int charCaseNum = Integer.parseInt(charCase);
+			result = result + charCaseNum;
 		}
 		return result;
 	}
 	
 	 public int getPalindrom() {
-		 int maxPar = 0;
-		    //int showNum1= 0;
-		    //int showNum2 = 0;
+		 int maxPalindrome = 0;
+		    //int palindromeMultiplier1= 0;
+		    //int palindromeMultiplier2 = 0;
 		    for (int i = 1000; i < 10000; i++) {
 				for (int j = 1000; j < 10000; j++) {
-					int result = i*j;
-					int check = 0;
-					String str = Integer.toString(result);
-					char [] chars = str.toCharArray();
-					String str2 = Character.toString(chars[chars.length-1]);
+					int productResult = i*j;
+					int palindromCheck = 0;
+					String productResultToString = Integer.toString(productResult);
+					char [] chars = productResultToString.toCharArray();
+					String productResultToStringReverse = Character.toString(chars[chars.length-1]);
 					for (int k = chars.length -2; k >= 0; k--) {
-						String ch = Character.toString(chars[k]);
-						str2 = str2 + ch;
+						String getArrayValue = Character.toString(chars[k]);
+						productResultToStringReverse = productResultToStringReverse + getArrayValue;
 					}
-					int result2 = Integer.parseInt(str2);
-					if(result == result2) {
-						check = result;
+					int productResultReverse = Integer.parseInt(productResultToStringReverse);
+					if(productResult == productResultReverse) {
+						palindromCheck = productResult;
 					}
-					if(maxPar< check) {
-						maxPar = check;
-						//showNum1 = i;
-						//showNum2 = j;
-					}
+					if(maxPalindrome< palindromCheck) {
+						maxPalindrome = palindromCheck;
+						//palindromeMultiplier1 = i;
+						//palindromeMultiplier2 = j;
+					} 
 					
 					}
 			}
-		   // System.out.println("Max palindrom is " + maxPar + " " + 
-		   //"with multiplier " + showNum1 + " and  " + showNum2);
-		    return maxPar;
+		   // System.out.println("Max palindrom is " + maxPalindrome + " " + 
+		   //"with multiplier " + palindromeMultiplier1 + " and  " + palindromeMultiplier2);
+		   return maxPalindrome; 
 	 }
 	
 }
