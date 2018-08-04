33baff981adf5d76ad7852d508a6deacc614559c
Vova <Vova@192.168.0.101>
2018-08-04 21:05:16 +0300

    add getPalindrome class and methods(NumOperations2)

---
+++ Lesson1/src/com/lessons/MainNumSum2.java
@@ -0,0 +1,18 @@
+package com.lessons;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.lessons.numoperations2.NumOperations2;
+
+public class MainNumSum2 {
+	private static Logger logger = Logger.getLogger(Main.class.getName());
+	public static void main(String[] args) {
+		NumOperations2 operations = new NumOperations2();
+		operations.getPalindromToList();
+		logger.log(Level.INFO," Max palindrome is " + operations.getMaxPalindrome());
+		logger.log(Level.INFO," Min palindrome is " + operations.getMinPalindrome());
+	    
+	}
+
+}
